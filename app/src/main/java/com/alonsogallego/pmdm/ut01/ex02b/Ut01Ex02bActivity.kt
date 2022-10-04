9cca1094295069a762e3da8f2a72840c4728bf25
alonsogallego7 <alonso.galpac@educa.jcyl.es>
2022-10-04 16:20:49 +0200

    Ejercicios de LinearLayout

---
+++ app/src/main/java/com/alonsogallego/pmdm/ut01/ex02b/Ut01Ex02bActivity.kt
@@ -0,0 +1,12 @@
+package com.alonsogallego.pmdm.ut01.ex02b
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alonsogallego.pmdm.R
+
+class Ut01Ex02bActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01_ex02b)
+    }
+}
