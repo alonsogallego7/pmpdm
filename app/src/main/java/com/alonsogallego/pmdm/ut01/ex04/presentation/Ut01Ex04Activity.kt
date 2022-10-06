e0e6d44468881605784843ea7019f74c2b64a253
alonsogallego7 <alonso.galpac@educa.jcyl.es>
2022-10-06 17:06:52 +0200

    exercise 4

---
+++ app/src/main/java/com/alonsogallego/pmdm/ut01/ex04/presentation/Ut01Ex04Activity.kt
@@ -0,0 +1,12 @@
+package com.alonsogallego.pmdm.ut01.ex04.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alonsogallego.pmdm.R
+
+class Ut01Ex04Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01_ex04)
+    }
+}
