3e54611cecaada7bd29a568cb2aa3f3b565ed2cb
alonsogallego7 <alonso.galpac@educa.jcyl.es>
2022-10-25 16:14:02 +0200

    ejercicio 7

---
+++ app/src/main/java/com/alonsogallego/pmdm/ut01/ex07/activity_ut01_ex07.kt
@@ -0,0 +1,12 @@
+package com.alonsogallego.pmdm.ut01.ex07
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alonsogallego.pmdm.R
+
+class activity_ut01_ex07 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01_ex07)
+    }
+}
